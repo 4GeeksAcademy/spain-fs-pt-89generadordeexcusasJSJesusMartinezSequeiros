1ef88536674a6ac2bb7a884e8e51e9b8f1ec74cc
Jesús Martínez Sequeiros <165663442+Jesus2508@users.noreply.github.com>
2025-01-13 11:11:38 +0000

    excusa definitiva

--- src/app.js
+++ src/app.js
@@ -5,7 +5,39 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ["El perro", "Mi abuela", "El cartero", "Mi pájaro"];
+let action = ["se comió", "orinó en", "aplastó", "rompió"];
+let what = ["mi tarea", "mi teléfono", "el coche", "las llaves"];
+let when = [
+  "antes de la clase",
+  "mientras dormía",
+  "mientras hacía ejercicio",
+  "durante mi almuerzo",
+  "mientras rezaba"
+];
+
+function generateExcuse() {
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    what[whatIndex] +
+    " " +
+    when[whenIndex]
+  );
+}
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let excuseElement = document.getElementById("excuse");
+  excuseElement.innerHTML = generateExcuse();
+
+  let button = document.getElementById("generateBtn");
+  button.addEventListener("click", function() {
+    excuseElement.innerHTML = generateExcuse();
+  });
 };
